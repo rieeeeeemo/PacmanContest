c34e84b4d9c930fc768918171a9cb6b481ed23ee
uncleleng <499537997@qq.com>
2017-09-30 12:13:16 +1000

    initialize

--- Desktop/contest/testClasses.py
+++ Desktop/contest/testClasses.py
@@ -22,7 +22,6 @@ import sys
 # maximum number of points they are worth, and are composed of a series of
 # test cases
 class Question(object):
-
     def raiseNotDefined(self):
         print 'Method not implemented: %s' % inspect.stack()[1][3]
         sys.exit(1)
@@ -46,9 +45,9 @@ class Question(object):
     def execute(self, grades):
         self.raiseNotDefined()
 
+
 # Question in which all test cases must be passed in order to receive credit
 class PassAllTestsQuestion(Question):
-
     def execute(self, grades):
         # TODO: is this the right way to use grades?  The autograder doesn't seem to use it.
         testsFailed = False
@@ -65,7 +64,6 @@ class PassAllTestsQuestion(Question):
 # Question in which predict credit is given for test cases with a ``points'' property.
 # All other tests are mandatory and must be passed.
 class HackedPartialCreditQuestion(Question):
-
     def execute(self, grades):
         # TODO: is this the right way to use grades?  The autograder doesn't seem to use it.
         grades.assignZeroCredit()
@@ -99,6 +97,7 @@ class Q6PartialCreditQuestion(Question):
         if False in results:
             grades.assignZeroCredit()
 
+
 class PartialCreditQuestion(Question):
     """Fails any test which returns False, otherwise doesn't effect the grades object.
     Partial credit tests will add the required points."""
@@ -113,7 +112,6 @@ class PartialCreditQuestion(Question):
                 return False
 
 
-
 class NumberPassedQuestion(Question):
     """Grade is the number of test cases passed."""
 
@@ -121,12 +119,8 @@ class NumberPassedQuestion(Question):
         grades.addPoints([f(grades) for _, f in self.testCases].count(True))
 
 
-
-
-
 # Template modeling a generic test case
 class TestCase(object):
-
     def raiseNotDefined(self):
         print 'Method not implemented: %s' % inspect.stack()[1][3]
         sys.exit(1)
@@ -175,7 +169,8 @@ class TestCase(object):
         extraCredit = max(0, points - maxPoints)
         regularCredit = points - extraCredit
 
-        grades.addMessage('%s: %s (%s of %s points)' % ("PASS" if points >= maxPoints else "FAIL", self.path, regularCredit, maxPoints))
+        grades.addMessage('%s: %s (%s of %s points)' % (
+        "PASS" if points >= maxPoints else "FAIL", self.path, regularCredit, maxPoints))
         if extraCredit > 0:
             grades.addMessage('EXTRA CREDIT: %s points' % (extraCredit,))
 
@@ -186,4 +181,3 @@ class TestCase(object):
 
     def addMessage(self, message):
         self.messages.extend(message.split('\n'))
-
